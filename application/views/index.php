5ab6e69992edf29020cdb87cc08623e2c3577ce9
andreyaici3 <andreyandri90@gmail.com>
2020-06-04 21:43:10 +0700

    masuk ke repo

---
+++ application/views/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Andrey Store adalah situs milik Andrey komputer yang berjalan dalam bidang Pulsa Kuota dan Pembayaran PPOB. Hingga saat ini ada banyak member yang sudah bergabung dengan andrey komputer">
+		<meta name="author" content="Andrey Andriansyah">
+		<meta name="robots" content="index, follow">
+		<meta name="format-detection" content="telephone=no" />
+		<!--<meta name="google-site-verification" content="G-CZ1-YNpQAYek-deTLU0Xaa2YxULE6d6F28pLlvHkY" />-->
+		<!-- <meta name="google-site-verification" content="-m0jIII4cznGDTav5ITGk6WWBgYv2LV-Vsi9br2fCEE" /> -->
+		<title>Andrey Store - Main Page</title>
+		<!-- Loading third party fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700" rel="stylesheet" type="text/css">
+		<!-- <link href="<?= base_url() ?>assets/font/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="<?= base_url() ?>assets/css/animate.css">
+		<link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+		<link rel="stylesheet" href="<?= base_url() ?>assets/css/custom.css">
+		<link rel="icon" type="image/png" href="kmsp-favicon.ico" />
+
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144237878-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-144237878-1');
+		</script>
+	</head>
+	<body>
+		<div id="site-content">
+			<header class="site-header">
+				<div class="top-header">
+					<div class="container">
+						<a>WA ONLY: 0896-7567-7955</a>
+						
+						<nav class="member-navigation pull-right">
+							<a href="" target="_blank"><i class="fa fa-user"></i> Member Login</a>
+						</nav> <!-- .member-navigation -->
+					</div> <!-- .container -->
+				 </div> <!-- .top-header -->
+
+				<div class="bottom-header">
+					<div class="container">
+						<a href="/" class="branding pull-left">
+							<img src="<?= base_url() ?>assets/img/logo.png" width="130" height="50" alt="Site title" title="Andrey Store | Andrey Komputer" class="logo-icon">
+						</a> <!-- #branding -->
+						
+						<nav class="main-navigation pull-right">
+							<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+							<ul class="menu">
+								<li class="menu-item"><a href="/">Beranda</a></li>
+								<li class="menu-item"><a href="#list-harga">Harga</a></li>
+								<li class="menu-item"><a href="" target="_blank">Testimoni</a></li>
+								<li class="menu-item"><a href="" target="_blank">Hubungi</a></li>
+								<li class="menu-item"><a href="order">Cara Pendaftaran</a></li>
+								<li class="menu-item"><a href="downloads">Syarat Dan Kebijakan</a></li>
+							</ul>
+						</nav> <!-- .main-navigation -->
+					</div> <!-- .container -->
+				</div> <!-- .bottom-header -->
+			</header> <!-- .site-header -->
+			<main class="content">
+				<div class="slider">
+					<ul class="slides">
+						<li>
+							<div class="container">
+								<img src="https://www.kmsp-store.com/assets/images/server-logo-kmsp-1.png" alt="server-logo-kmsp-1.png">
+								<div class="slide-caption">
+									<h2 class="slide-title">Andrey Store</h2>
+									<small class="slide-subtitle">Penyedia Jasa Isi Ulang Pulsa dan Kuota</small>
+									<div class="slide-summary">
+										<p align="justify">Andrey Store adalah Resseler Sekaligus Distributor penyedia Jasa pengisian pulsa dan kuota All Operator, Tanpa Perlu takut Kehabisan pulsa dan kuota anda, belum punya uang.? andrey komputer menyediakan sistem hutang. punya uang lebih? bisa sistem deposit. segalanya mudah dengan andrey Store</p>
+									</div>
+									<a href="#list-harga" class="button">Lihat Detail Harga</a>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="container">
+								<img src="https://www.kmsp-store.com/assets/images/server-logo-kmsp-1.png" alt="server-logo-kmsp-1.png">
+								<div class="slide-caption">
+									<h2 class="slide-title">Penyedia Jasa Service</h2>
+									<small class="slide-subtitle">Terima Service Printer, PC dan Laptop</small>
+									<div class="slide-summary">
+										<p align="justify">Menerima jasa service Printer, PC dan laptop termasuk install ulang, install software, isi game dsb. Silahkan bagi yang membutuhkan jasa service komputer bisa berkunjung ke rumah saya yang beralamatkan di dusun puhun RT01/02 desa Cikubangmulya. Kec. Ciawigebang, Kab. Kuningan - 45591</p>
+									</div>
+									<a href="#list-harga" class="button">Lihat Detail Harga</a>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="container">
+								<img src="https://www.kmsp-store.com/assets/images/server-logo-kmsp-1.png" alt="server-logo-kmsp-1.png">
+									<h2 class="slide-title">Penyedia Jasa Percetakan</h2>
+									<small class="slide-subtitle">Terima Segala bentuk cetakan</small>
+									<div class="slide-summary">
+										<p align="justify">Menerima jasa cetak undangan, amplop nama, kartu nama, kartu lahiran / syukuran / akikah, label dengan nama untuk undangan, spanduk dan lainnya, terima juga jasa setting atau pun tanpa setting. harga bersahabat dengan kantong anda.</p>
+									</div>
+									<a href="#list-harga-2" class="button">Lihat Detail Harga</a>
+								</div>
+							</div>
+						</li>
+					</ul> <!-- .slides -->
+				</div> <!-- .slider -->
+		</div>
+
+		<script src="<?= base_url() ?>assets/js/jquery-1.11.1.min.js"></script>
+		<script src="<?= base_url() ?>assets/js/plugins.js"></script>
+		<script src="<?= base_url() ?>assets/js/app.js"></script>
+	</body>
+</html>
